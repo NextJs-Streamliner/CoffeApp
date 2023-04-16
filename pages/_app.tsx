1be8b186243a36ba18e9510b2ce4bf354c082b13
tiger <thisisstreamliner@gmail.com>
2023-04-16 20:58:11 +0300

    Remove Unused Code From Home Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Footer from './footer.js'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <Footer/>
+    </div>
+  )
 }
---
+++ pages/footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+        <footer>2023 ThisIsStreamliner</footer>
+    )
+}
+
+export default Footer;
