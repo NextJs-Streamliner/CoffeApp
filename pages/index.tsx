8d6abb42f91469cda0a18c2500e1a5f3168d6fdd
tiger <thisisstreamliner@gmail.com>
2023-05-13 20:53:34 +0300

    Add Coffee Stores on Page. Fix next.config.js to use Unsplash Image

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Card from '../components/card'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import coffeeStores from '../data/coffee-stores.json'
+
 export default function Home() {
 
   const handleOnBannerBtnClick = () => {
@@ -22,18 +24,14 @@ export default function Home() {
           <Image src="/static/hero-image.png" width={700} height={400} alt="coffee"/>
         </div>
         <div className={styles.cardLayout}>
-          <Card 
-            className={styles.card}
-            name='DarkHouse Coffee' 
-            imgUrl="/static/hero-image.png" 
-            href="/coffee-store/darkhouse-coffee"
-          />
-          <Card 
-            className={styles.card}
-            name='DarkHouse Coffee' 
-            imgUrl="/static/hero-image.png" 
-            href="/coffee-store/darkhouse-coffee"
-          />
+          {coffeeStores.map((coffeeStore) => {
+            return (<Card 
+              className={styles.card}
+              name={coffeeStore.name} 
+              imgUrl={coffeeStore.imgUrl}
+              href={`/coffee-store/${coffeeStore.id}`}
+            />)
+          })}
         </div>
       </main>
     </div>
