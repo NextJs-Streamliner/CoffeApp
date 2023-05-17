197bd0e232b742b54a0b823bc7a299a7e3a79d0d
tiger <thisisstreamliner@gmail.com>
2023-05-17 19:55:42 +0300

    Fix getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -7,7 +7,7 @@ export function getStaticProps(staticProps) {
     return {
         props: {
             "coffeeStore" : coffeeStoreData.find(coffeeStore => {
-                return coffeeStore.id === 0//params.id // dynamic id
+                return coffeeStore.id.toString() === params.id // dynamic id
             })
         }
     }
@@ -17,19 +17,22 @@ export function getStaticPaths() {
     return {
         paths: [
             { params: { id: '0'} },
-            { params: { id: '1'} }
-        ]
+            { params: { id: '1'} },
+            { params: { id: '300'} }
+        ],
+        fallback: false
     }
 }
 
-const CoffeeStore = () => {
+const CoffeeStore = (props) => {
     const router = useRouter()
-
+    console.log('props', props)
     return (
         <div>
             Coffee Store Page {router.query.id} 
             <Link href="/">Back to Home</Link>
             <Link href="/coffee-store/dynamic">Go to page dynamic</Link>
+            <p>{props.coffeeStore.name}</p>
         </div>
     )
 }
